96033138b695ebc7766df2e97540e0e93d29635d
harshitit <harshitthakur1101@gmail.com>
2017-07-21 12:59:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
 var ngTodo = angular.module('ngTodo', []);
-console.log(ngTodo)
+
+ngTodo.controller('mainController',function($scope){
+	$scope.Lists=[
+	{
+		title:'Do my Homework',
+		details:'I must do my home work today'
+	},
+	{
+		title:'Do my Assignment',
+		details:'I must do my assignment tommorow'
+	},
+	{
+		title:'Do Angular',
+		details:'I must do anguler today'
+	},
+	{
+		title:'Do Angular Test',
+		details:'I must do complete my anguler test today'
+	},
+	{
+		title:'Going Home',
+		details:'Today i go back to my hometown'
+	}]
+	$scope.addTodo = function(title,details){
+		$scope.Lists.push({title,details});
+	}
+	$scope.deleteTodo= function(key){
+		var index = $scope.Lists.indexOf(key);
+		$scope.Lists.splice(index, 1);
+	}
+})
